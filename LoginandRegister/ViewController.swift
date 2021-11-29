21bfb3bd3f0955a60b83d27560377ef418edbf97
saman <samanpreetkaur.881995@gmail.com>
2021-11-29 07:17:52 -0800

    radius value decreased

--- LoginandRegister/ViewController.swift
+++ LoginandRegister/ViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var UserTextField: UITextField!
+    
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    var radius = 21
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        
+        UserTextField.layer.cornerRadius = CGFloat(radius)
+        
+        PasswordTextField.layer.cornerRadius = CGFloat(radius)
+        
+        loginButton.layer.cornerRadius = CGFloat(radius)    }
 
 
 }
